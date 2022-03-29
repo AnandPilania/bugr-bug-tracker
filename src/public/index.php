8863e57d1ecd60209a853726151b6fdee6d11e4e
Rob Watson <rob.watson@me.com>
2022-03-29 20:35:36 +0100

    Initial commit of some code

---
+++ src/public/index.php
@@ -0,0 +1,5 @@
+<?php
+
+include dirname(__DIR__).'/resources/template/html_head.tpl';
+include dirname(__DIR__).'/resources/template/pages/index.tpl';
+include dirname(__DIR__).'/resources/template/html_foot.tpl';
---
+++ src/resources/template/html_head.tpl
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html lang="en-gb">
+    <head>
+        <title>Bugr - A Bug tracker</title>
+        <link rel="stylesheet" type="text/css" href="/css/main.css" />
+    </head>
+    <body>
+
