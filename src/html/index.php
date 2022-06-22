44d1f327d1fe998b21023db6d4eca4188427857b
Rob Watson <rob.watson@me.com>
2022-06-22 18:52:54 +0100

    Update public files, add example config file

--- src/html/index.php
+++ src/html/index.php
@@ -1,14 +1,15 @@
 <?php
 
-// setup autoloader
+require dirname(__DIR__).'/resources/lib/autoloader.php';
 
-// start the core
+$core = SourcePot\Core\Core::create()->execute();
 
-// tell core to execute request
+echo "\n\nAutoloaded classes:\n";
+echo implode("\n", Autoloader::$loadedClasses);
 
-$password = trim(file_get_contents(dirname(__DIR__).'/resources/database_password'));
-$db = new PDO('mysql:host=bugr-mysql', 'bugr', $password);
-$db->query('USE bugr');
-var_dump($db->query('SHOW TABLES')->fetchAll());
+// $password = trim(file_get_contents(dirname(__DIR__).'/resources/database_password'));
+// $db = new PDO('mysql:host=bugr-mysql', 'bugr', $password);
+// $db->query('USE bugr');
+// var_dump($db->query('SHOW TABLES')->fetchAll());
 
 // phpinfo();
